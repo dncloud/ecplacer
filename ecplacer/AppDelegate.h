1bcf43c66b3bb6bb8e6154b8c3ec7aa245582fda
Oliver Cermann <oliver.cermann@devicenull.de>
2018-07-13 11:22:59 +0200

    Initial Commit

---
+++ ecplacer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ecplacer
+//
+//  Created by Oliver Cermann on 13.07.18.
+//  Copyright © 2018 Oliver Cermann. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ecplacer/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ecplacer
+//
+//  Created by Oliver Cermann on 13.07.18.
+//  Copyright © 2018 Oliver Cermann. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
